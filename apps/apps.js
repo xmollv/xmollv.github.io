eb8c0806dde59acaa199909d32617c6e636d8c57
Xavi Moll <xmollv@gmail.com>
2026-03-20 16:37:28 +0100

    Added an apps section

---
+++ apps/apps.js
@@ -0,0 +1,26 @@
+const apps = [
+    {
+        "name": "Later",
+        "description": "You don’t have time to read everything the internet throws your way. That’s where Later comes in—the sleek, powerful way to save links of articles, videos, shopping items, etc to read on your terms. No ads. No accounts. No tracking. Just pure, private saving since 2020. Whether it's a deep-dive article, a how-to guide, or something you know you'll want later, Later is your perfect companion across iPhone, iPad, and Mac.",
+        "icon": "later.png",
+        "url": "https://apps.apple.com/app/id1507396839",
+        "status": "Active",
+        "statusColor": "#34C759"
+    },
+    {
+        "name": "Bookie",
+        "description": "Bookie allows you to keep track of your reading journey. It allows you to save the books that you're planning on reading, those who you're currently reading and those who you've already read.",
+        "icon": "bookie.png",
+        "url": "https://apps.apple.com/app/id1553698241",
+        "status": "Maintenance",
+        "statusColor": "#FF9500"
+    },
+    {
+        "name": "Llitgi",
+        "description": "Llitgi is the fastest Pocket client out there. Everything that you do in Llitgi is synced with your Pocket account, which means that everything is available on your iPhone, iPad or Mac.",
+        "icon": "llitgi.png",
+        "url": "https://apps.apple.com/app/id1355340767",
+        "status": "Unmaintained",
+        "statusColor": "#FF3B30"
+    }
+];
